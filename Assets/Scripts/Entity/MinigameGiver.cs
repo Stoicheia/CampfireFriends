9c50f1aa155def8ace90ae4b7f3b782575a3623e
Stoicheia <37011850+Stoicheia@users.noreply.github.com>
2022-12-31 08:21:13 +1100

    f

---
+++ Assets/Scripts/Entity/MinigameGiver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Minigame;
+using UnityEngine;
+
+namespace Entity
+{
+    [RequireComponent(typeof(DialogeMan))]
+    public class MinigameGiver : MonoBehaviour
+    {
+        [SerializeField] private MinigameDefinition _myMinigame;
+        private DialogeMan _dialoge;
+
+        private void Awake()
+        {
+            _dialoge = GetComponent<DialogeMan>();
+        }
+    }
+}
