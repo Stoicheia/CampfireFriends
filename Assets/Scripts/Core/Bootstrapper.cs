24cd58dd94c6442d2d86990313fcea26f29f54b5
Stoicheia <37011850+Stoicheia@users.noreply.github.com>
2022-12-31 11:22:22 +1100

    bootstrapper

---
+++ Assets/Scripts/Core/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Core
+{
+    public class Bootstrapper : MonoBehaviour
+    {
+        [SerializeField] private List<string> _sceneNames;
+        private void Start()
+        {
+            foreach(var n in _sceneNames)
+            {
+                SceneManager.LoadScene(n, LoadSceneMode.Additive);
+            }
+        }
+    }
+}
